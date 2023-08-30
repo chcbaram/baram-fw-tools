a47229cd748ce421542d2424b4d3a70fa760970a
Hancheol Cho <chcbaram@gmail.com>
2023-08-30 20:33:29 +0900

    ESP-IDF 추가

---
+++ utils.py
@@ -0,0 +1,25 @@
+import os
+import git
+import stat
+from git import RemoteProgress
+from tqdm import tqdm
+
+
+class CloneProgress(RemoteProgress):
+    def __init__(self):
+        super().__init__()
+        self.pbar = tqdm()
+
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        self.pbar.total = max_count
+        self.pbar.n = cur_count
+        self.pbar.refresh()
+
+def make_dir_writable(function, path, exception):
+    """The path on Windows cannot be gracefully removed due to being read-only,
+    so we make the directory writable on a failure and retry the original function.
+    """
+    os.chmod(path, stat.S_IWRITE)
+    function(path)
+
+
